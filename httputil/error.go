a87a8011849ce382245f5212318a85618ff07f95
pei0804 <peeeei0804@gmail.com>
2017-11-09 11:06:31 +0900

    update: httperror

--- httputil/error.go
+++ httputil/error.go
@@ -12,6 +12,9 @@ func (he *HTTPError) Error() string {
 }
 
 func NewHTTPError(status int, message ...interface{}) *HTTPError {
-	he := &HTTPError{Status: status}
+	he := &HTTPError{Status: status, Message: string(status)}
+	if len(message) > 0 {
+		he.Message = fmt.Sprint(message...)
+	}
 	return he
 }

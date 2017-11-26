b6ff7ed09d18517a94de36e538e4ecde1e7e1fda
pei0804 <peeeei0804@gmail.com>
2017-11-27 00:31:44 +0900

    fix: Auth Header

--- 01/main.go
+++ 01/main.go
@@ -61,7 +61,7 @@ func (s *Server) Router() {
 func Auth(db string) (fn func(http.Handler) http.Handler) {
 	fn = func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			token := r.Header.Get("Auth")
+			token := r.Header.Get("Authorization")
 			if token != "admin" {
 				respondError(w, http.StatusUnauthorized, fmt.Errorf("利用権限がありません"))
 				return
